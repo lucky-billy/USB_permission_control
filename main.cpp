4e58bf7390ae28e7f7ba5d9efe1577107e5df8a7
QY-Admin <1623812580@qq.com>
2021-11-01 13:31:05 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <QSettings>
+#include <QDebug>
+
+int main()
+{
+    QSettings setting("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\USBSTOR", QSettings::NativeFormat);
+    setting.setValue("Start", 3);
+//    setting.setValue("Start", 4);
+    return 0;
+}
